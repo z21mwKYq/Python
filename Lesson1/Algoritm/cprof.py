74026a1e134e7454d7aa140d31b8ea90ddbcf248
Vladimir <vladimir-i2005@yandex.ru>
2020-04-15 23:49:34 +0300

    New theme

---
+++ Lesson1/Algoritm/cprof.py
@@ -0,0 +1,17 @@
+import cProfile
+
+def get_len(array):
+    return  len(array)
+
+def get_sum(array):
+    S = 0
+    for i in array:
+        S +=i
+    return S
+
+def main ():
+    lst = [i for i in range (1000000)]
+    a = get_len(lst)
+    b = get_sum(lst)
+
+cProfile.run('main()')
